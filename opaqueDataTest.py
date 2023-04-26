e746dab79645ddb63a428bcaa240eea4087c5827
Mark IJzerman <info@markijzerman.com>
2023-04-26 16:19:24 +0200

    stay on when user is logged in

--- opaqueDataTest.py
+++ opaqueDataTest.py
@@ -1,13 +1,44 @@
 #!/usr/bin/python3
 
-import os
+import subprocess, os
+import atexit
 import logging
 from time import sleep
 from pijuice import PiJuice
 
-# for picture date getting
+# VALUES
+time = 20
+
+# for internet connection check
+import urllib.request
+
+# for picture date getting 
 from datetime import datetime
 
+# for checkin internet connection
+def connect(host='http://google.com'):
+    try:
+        urllib.request.urlopen(host) #Python 3.x
+        return True
+    except:
+        return False
+    
+# for checking if user is logged on
+def checkForUser():
+    piUserList = subprocess.check_output('users', shell=True).rstrip()
+    if piUserList:
+        logging.info('user is logged in, staying on!')
+        print('user is logged on, staying on')
+        return True
+    else:
+        logging.info('no users logged in, turning off...')
+        return False
+    
+# called when program exits
+def exit_handler():
+    # Make sure wakeup_enabled and wakeup_on_charge have the correct values
+    pj.rtcAlarm.SetWakeupEnabled(True)
+    pj.power.SetWakeUpOnCharge(0)
 
 logging.basicConfig(
 	filename = '/home/opaque/opaqueoceans/pistatus.log',
@@ -25,41 +56,43 @@ while pjOK == False:
    else:
       sleep(0.1)
 
-# If on battery power, shut down after 3min
-data = stat['data']
-if data['powerInput'] == "NOT_PRESENT" or "BAD" and data['powerInput5vIo'] == 'NOT_PRESENT' or "BAD":
 
-    print('turning off in 10m, on battery')
 
-    # print data for checking
-    print(stat['data'])
+# print data for checking
+print(stat['data'])
 
-	# Write statement to log
-    logging.info('Hello! Raspberry Pi on battery power. Turning off in 10min - taking picture')
+# Write statement to log
+logging.info('Hello! Raspberry Pi on battery power. Taking picture!')
 
-    os.system("/usr/bin/libcamera-still -o /home/opaque/opaqueoceans/images/image" + str(datetime.now().strftime('%Y-%m-%d-%H-%M-%S')) + ".jpg")
+curDate = str(datetime.now().strftime('%Y-%m-%d-%H-%M-%S'))
+os.system("/usr/bin/libcamera-still -o /home/opaque/opaqueoceans/images/image" + curDate + ".jpg")
 
-   # Keep Raspberry Pi running - THING TO DO WOULD BE HERE, WOULD TURN OFF AFTER THIS!
-    sleep(600)
+# if internet, upload image
+# !! CHANGE THIS TO UPLOAD ANY THAT HAVE NOT BEEN UPLOADED !!
+if connect() == True:
+    print('Internet is connected! Uploading image!')
+    os.system("/home/opaque/opaqueoceans/dropbox_uploader.sh upload /home/opaque/opaqueoceans/images/image" + curDate + ".jpg /")
+else:
+    print('no internet, uploading later')
 
-   # Make sure wakeup_enabled and wakeup_on_charge have the correct values
+# check if there is a user logged on, if so stay on, if not, turn off
+# Keep Raspberry Pi running - THING TO DO WOULD BE HERE, WOULD TURN OFF AFTER THIS!
+if checkForUser() == False:
+    sleep(time)
+    
+    # Make sure wakeup_enabled and wakeup_on_charge have the correct values
     pj.rtcAlarm.SetWakeupEnabled(True)
     pj.power.SetWakeUpOnCharge(0)
 
-   # Make sure power to the Raspberry Pi is stopped to not deplete
-   # the battery
+    # Make sure power to the Raspberry Pi is stopped to not deplete
+    # the battery
     pj.power.SetSystemPowerSwitch(0)
     pj.power.SetPowerOff(30)
 
-    logging.info('Bye! shutting down now as minutes have elapsed')
+    logging.info('Bye! shutting down now as time has elapsed')
 
-   # Now turn off the system
+    # Now turn off the system
     os.system("sudo shutdown -h now")
 
-else:
-    # print data for checking
-    print(stat['data'])
-    print('staying on, mains power')
-
-	# Write statement to log
-    logging.info('Raspberry Pi on mains power, not turned off automatically')
+atexit.register(exit_handler)
+sleep(60)
