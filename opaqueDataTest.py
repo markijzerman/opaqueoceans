3be39d45b5f720fc955a3b5fe2a4d4f63d8d1fbd
Loden <l.rietveld@hotmail.com>
2023-05-12 12:41:52 +0000

    Add configurable alarms
    Add config.json with configurable day period, timed alarms
    (array with wakeup times), periodic alarms (every x hours)
    with start and end times
    
    Change order of operations in README, disable timesync after enabling
    real hwclock so the rtc syncs

--- opaqueDataTest.py
+++ opaqueDataTest.py
@@ -6,7 +6,10 @@ import logging
 from time import sleep
 from pijuice import PiJuice
 
+import typing as tp
+
 import glob
+import json
 
 # VALUES
 time = 20
@@ -15,7 +18,7 @@ time = 20
 import urllib.request
 
 # for picture date getting 
-from datetime import datetime
+from datetime import datetime, timedelta
 
 # for checkin internet connection
 def connect(host='http://google.com'):
@@ -35,11 +38,119 @@ def checkForUser():
     else:
         logging.info('no users logged in, turning off...')
         return False
+
+def get_alarm_config() -> dict:
+    config_path = "/home/opaque/opaqueoceans/config.json"
+    if os.path.exists(config_path):
+        with open(config_path, "r") as f:
+            try:
+                return json.load(f)['alarm_config']
+            except Exception as e:
+                logging.warning(f"Failed to load alarm config: {e}")
     
+    return None
+
+def get_hour_minute_tuple_from_str(time: str) -> tp.Tuple[int, int]:
+    times = time.split(":")
+    return (int(times[0]), int(times[1]))
+
+def get_time_dt_obj_from_time_str(time: str) -> timedelta:
+    times = get_hour_minute_tuple_from_str(time)
+    return timedelta(hours=times[0], minutes=times[1])
+
+def get_hour_minute_tuple_from_time_dt(time: timedelta) -> tp.Tuple[int, int]:
+    hour = (time.seconds // 3600) % 24
+    minute = (time.seconds // 60) % 60
+    return (hour, minute)
+
+# Week starts on sunday because US of A, range is 1-7
+def get_weekday_from_period(period: int, current_weekday: int) -> int:
+    return ((current_weekday - 1 + period) % 7) + 1
+
+
+def get_next_periodic_alarm_time(cfg: dict, time: timedelta, current_weekday: int) -> tp.Tuple[int, int, int]:
+    p_cfg = cfg['periodic_config']
+
+    logging.info("Getting periodic alarm time...")
+    start_time = get_time_dt_obj_from_time_str(p_cfg['start_time'])
+    end_time = get_time_dt_obj_from_time_str(p_cfg['end_time'])
+    hm_period = get_time_dt_obj_from_time_str(p_cfg['hm_period'])
+    
+    # if we pass the end-time by incrementing with period, just start at the next day
+    if time + hm_period > end_time:
+        new_weekday = get_weekday_from_period(cfg['day_period'], current_weekday)
+        hour,minute = get_hour_minute_tuple_from_time_dt(start_time)
+        return (new_weekday, hour, minute)
+    else:
+        new_weekday = current_weekday
+        hour,minute = get_hour_minute_tuple_from_time_dt(time + hm_period)
+        return (new_weekday, hour, minute)
+
+def get_next_timed_alarm_time(cfg: dict, current_time: dict, pj: PiJuice) -> tp.Tuple[int, int, int]:
+    logging.info("Getting timed alarm time...")
+    prev_alarm = pj.rtcAlarm.GetAlarm()
+    prev_alarm = f"{prev_alarm['data']['hour']}:{prev_alarm['data']['minute']}"
+
+    prev_alarm_idx = -1
+
+    try:
+        prev_alarm_idx = cfg['timed_config'].index(prev_alarm)
+    except Exception as e:
+        logging.warning("Could not find previous alarm time in timed_config, comparing time instead")
+        current_dt = timedelta(hours=current_time['hour'], minutes=current_time['minute'])
+        for i, time in enumerate(cfg['timed_config']):
+            hour,minute = get_hour_minute_tuple_from_str(time)
+            # if the alarm time is smaller than current time, use that as previous time, 
+            if timedelta(hours=hour, minutes=minute) < current_dt:
+                prev_alarm_idx = i
+            else:
+                break
+        
+        # if we didn't find a smaller time, the current time is before the first alarm time, 
+        # so set previous time to the last alarm index
+        if prev_alarm_idx == -1:
+            prev_alarm_idx = len(cfg['timed_config'])
+    
+    next_alarm_idx = (prev_alarm_idx + 1) % len(cfg['timed_config'])
+    next_day_period = next_alarm_idx < prev_alarm_idx
+
+    new_weekday = current_time['weekday']
+    hour,minute = get_hour_minute_tuple_from_str(cfg['timed_config'][next_alarm_idx])
+    
+    if next_day_period:
+        new_weekday = get_weekday_from_period(cfg['day_period'], current_time['weekday'])
+
+    return (new_weekday, hour, minute)
+
+def get_next_time_from_config_and_current_time(cfg: dict, current_time: dict, pj: PiJuice) -> tp.Tuple[int, int, int]:
+    time = timedelta(hours=current_time['hour'], minutes=current_time['minute'])
+    if cfg['periodic_photos']:
+        return get_next_periodic_alarm_time(cfg, time, current_time['weekday'])
+    else:
+        return get_next_timed_alarm_time(cfg, current_time, pj)
+
+def set_alarm_from_config(pj: PiJuice) -> bool:
+    cfg = get_alarm_config()
+    time_dict = pj.rtcAlarm.GetTime()['data']
+
+    try:
+        weekday, hour, minute = get_next_time_from_config_and_current_time(cfg, time_dict, pj)
+        logging.info(f"Setting new alarm day, hour, minute to: {weekday}, {hour}, {minute}")
+        ret = pj.rtcAlarm.SetAlarm({"weekday" : weekday, "hour" : hour, "minute" : minute})
+        if ret['error'] != "NO_ERROR":
+            raise Exception(ret['error'])
+
+    except Exception as e:
+        logging.warning(f"Failed to set new alarm time from config. Using hardcoded time. Error was: {e}")
+        pj.rtcAlarm.SetAlarm({'hour' : 13, 'minute' : 0})
+        return False
+    
+    return True
+
 # called when program exits
 def exit_handler():
     # Make sure wakeup_enabled and wakeup_on_charge have the correct values
-    pj.rtcAlarm.SetAlarm({'minute_period': 6})
+    set_alarm_from_config(pj)
     pj.rtcAlarm.SetWakeupEnabled(True)
     #pj.power.SetWakeUpOnCharge(0)
     logging.shutdown()
@@ -163,8 +274,9 @@ if __name__ == "__main__":
             else:
                 logging.info("Uploading to dropbox failed. Retry next time internet connection is found.")
                 break
-
-        set_last_uploaded(upload_tracker, last_file)
+        
+        if last_file != "":
+            set_last_uploaded(upload_tracker, last_file)
 
     else:
         print('no internet, uploading later')
